c5dfa22b6f8f3611ee8e3f946a9f3523c1ce147c
hyobbang <nbgreen@naver.com>
2020-12-22 01:02:45 +0900

    tcp 통신 - 학번 보내기 & db연결

--- DB/Server/main.py
+++ DB/Server/main.py
@@ -1,6 +1,5 @@
 import tcpServer
 
-
 # server ip 주소 - ipconfig 명령으로 확인
-andRaspTCP = tcpServer.TCPServer('127.0.0.1', 9999)
+andRaspTCP = tcpServer.TCPServer('192.168.35.190', 9999)
 andRaspTCP.start()
