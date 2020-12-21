c185b2d74ffa5b48d212c2db2490dfb9b964777d
hyobbang <nbgreen@naver.com>
2020-12-21 15:21:49 +0900

    db설계 및 함수작성

---
+++ DB/DBconnection.py
@@ -0,0 +1,43 @@
+import numpy as np
+import datetime
+import pymysql
+
+
+#mysql 연결
+conn = pymysql.connect(
+    host = 'localhost', 
+    user ='Sejong', 
+    password = 'sejong2020H', 
+    db = 'exam',
+    charset = 'utf8'
+)
+
+#DB에 참조하기 위한 객체
+curs = conn.cursor(pymysql.cursors.DictCursor) 
+#curs = conn.cursor() 
+
+# 학생 이미지 select
+def load_studentdata(exam_id):
+    sql = "select ES.student_id, I.file from EXAM_STUDENT ES inner join IMAGE I on ES.student_id = I.student_id where ES.exam_id = %s" 
+    curs.execute(sql, exam_id)
+    conn.commit()
+    return curs.fetchall()
+
+# 클립보드 내용 저장
+def store_clipboard(exam_id, student_id, clipboard):
+    sql = "update EXAM_STUDENT set clipboard=%s where exam_id = %s and student_id = %s"
+    curs.execute(sql, (clipboard, exam_id, student_id))
+    conn.commit()
+    
+# 부정행위 시 face_log 기록 저장
+def store_facelog(exam_id, student_id, image, error_type, remarks):
+    sql = "insert into FACE_LOG (exam_id, student_id, time, error_type, image, remarks) values (%s, %s, now(), %s, %s, %s)"
+    curs.execute(sql, (exam_id, student_id, error_type, image, remarks))
+    conn.commit()
+
+data = load_studentdata(2)
+print(data)
+
+store_clipboard(1, 18011529, "어쩌구저쩌구")
+
+store_facelog(5, 18011529, "log_images/image1.jpg", 2, "three")
---
+++ DB/Exam_insert.sql
@@ -0,0 +1,120 @@
+
+insert into EXAM values (1, "미분적분학");
+insert into EXAM values (2, "선형대수및프로그래밍");
+insert into EXAM values (3, "C프로그래밍");
+insert into EXAM values (4, "Python");
+insert into EXAM values (5, "알고리즘");
+
+insert into STUDENT values (18011529, "김효경");
+insert into STUDENT values (18011111, "박태정");
+insert into STUDENT values (17012345, "김찬규");
+insert into STUDENT values (17098765, "목승주");
+insert into STUDENT values (20000000, "강동원");
+insert into STUDENT values (19022222, "배수지");
+insert into STUDENT values (16033333, "남주혁");
+insert into STUDENT values (15044444, "서현진");
+insert into STUDENT values (18055555, "유재석");
+insert into STUDENT values (17012121, "조이");
+insert into STUDENT values (14054545, "이재욱");
+
+insert into EXAM_STUDENT values (1, 18011529, "");
+insert into EXAM_STUDENT values (1, 20000000, "");
+insert into EXAM_STUDENT values (1, 19022222, "");
+insert into EXAM_STUDENT values (1, 15044444, "");
+insert into EXAM_STUDENT values (1, 14054545, "");
+
+insert into EXAM_STUDENT values (2, 18011529, "");
+insert into EXAM_STUDENT values (2, 18011111, "");
+insert into EXAM_STUDENT values (2, 17012345, "");
+insert into EXAM_STUDENT values (2, 17098765, "");
+insert into EXAM_STUDENT values (2, 19022222, "");
+
+insert into EXAM_STUDENT values (3, 18011529, "");
+insert into EXAM_STUDENT values (3, 17098765, "");
+insert into EXAM_STUDENT values (3, 20000000, "");
+insert into EXAM_STUDENT values (3, 16033333, "");
+insert into EXAM_STUDENT values (3, 17012121, "");
+
+insert into EXAM_STUDENT values (4, 18011529, "");
+insert into EXAM_STUDENT values (4, 18011111, "");
+insert into EXAM_STUDENT values (4, 17012345, "");
+insert into EXAM_STUDENT values (4, 15044444, "");
+insert into EXAM_STUDENT values (4, 18055555, "");
+
+insert into EXAM_STUDENT values (5, 18011529, "");
+insert into EXAM_STUDENT values (5, 18011111, "");
+insert into EXAM_STUDENT values (5, 20000000, "");
+insert into EXAM_STUDENT values (5, 18055555, "");
+insert into EXAM_STUDENT values (5, 14054545, "");
+
+insert into IMAGE values (1, 18011529, "images/18011529_1.jpg");
+insert into IMAGE values (2, 18011529, "images/18011529_2.jpg");
+insert into IMAGE values (3, 18011529, "images/18011529_3.jpg");
+insert into IMAGE values (4, 18011529, "images/18011529_4.jpg");
+insert into IMAGE values (5, 18011529, "images/18011529_5.jpg");
+
+insert into IMAGE values (6, 18011111, "images/18011111_1.jpg");
+insert into IMAGE values (7, 18011111, "images/18011111_2.jpg");
+insert into IMAGE values (8, 18011111, "images/18011111_3.jpg");
+insert into IMAGE values (9, 18011111, "images/18011111_4.jpg");
+insert into IMAGE values (10, 18011111, "images/18011111_5.jpg");
+
+insert into IMAGE values (11, 17012345, "images/17012345_1.jpg");
+insert into IMAGE values (12, 17012345, "images/17012345_2.jpg");
+insert into IMAGE values (13, 17012345, "images/17012345_3.jpg");
+insert into IMAGE values (14, 17012345, "images/17012345_4.jpg");
+insert into IMAGE values (15, 17012345, "images/17012345_5.jpg");
+
+insert into IMAGE values (16, 17098765, "images/17098765_1.jpg");
+insert into IMAGE values (17, 17098765, "images/17098765_2.jpg");
+insert into IMAGE values (18, 17098765, "images/17098765_3.jpg");
+insert into IMAGE values (19, 17098765, "images/17098765_4.jpg");
+insert into IMAGE values (20, 17098765, "images/17098765_5.jpg");
+
+insert into IMAGE values (21, 20000000, "images/20000000_1.jpg");
+insert into IMAGE values (22, 20000000, "images/20000000_2.jpg");
+insert into IMAGE values (23, 20000000, "images/20000000_3.jpg");
+insert into IMAGE values (24, 20000000, "images/20000000_4.jpg");
+insert into IMAGE values (25, 20000000, "images/20000000_5.jpg");
+
+insert into IMAGE values (26, 19022222, "images/19022222_1.jpg");
+insert into IMAGE values (27, 19022222, "images/19022222_2.jpg");
+insert into IMAGE values (28, 19022222, "images/19022222_3.jpg");
+insert into IMAGE values (29, 19022222, "images/19022222_4.jpg");
+insert into IMAGE values (30, 19022222, "images/19022222_5.jpg");
+
+insert into IMAGE values (31, 16033333, "images/16033333_1.jpg");
+insert into IMAGE values (32, 16033333, "images/16033333_2.jpg");
+insert into IMAGE values (33, 16033333, "images/16033333_3.jpg");
+insert into IMAGE values (34, 16033333, "images/16033333_4.jpg");
+insert into IMAGE values (35, 16033333, "images/16033333_5.jpg");
+
+insert into IMAGE values (36, 15044444, "images/15044444_1.jpg");
+insert into IMAGE values (37, 15044444, "images/15044444_2.jpg");
+insert into IMAGE values (38, 15044444, "images/15044444_3.jpg");
+insert into IMAGE values (39, 15044444, "images/15044444_4.jpg");
+insert into IMAGE values (40, 15044444, "images/15044444_5.jpg");
+
+insert into IMAGE values (41, 18055555, "images/18055555_1.jpg");
+insert into IMAGE values (42, 18055555, "images/18055555_2.jpg");
+insert into IMAGE values (43, 18055555, "images/18055555_3.jpg");
+insert into IMAGE values (44, 18055555, "images/18055555_4.jpg");
+insert into IMAGE values (45, 18055555, "images/18055555_5.jpg");
+
+insert into IMAGE values (46, 17012121, "images/17012121_1.jpg");
+insert into IMAGE values (47, 17012121, "images/17012121_2.jpg");
+insert into IMAGE values (48, 17012121, "images/17012121_3.jpg");
+insert into IMAGE values (49, 17012121, "images/17012121_4.jpg");
+insert into IMAGE values (50, 17012121, "images/17012121_5.jpg");
+
+insert into IMAGE values (51, 14054545, "images/14054545_1.jpg");
+insert into IMAGE values (52, 14054545, "images/14054545_2.jpg");
+insert into IMAGE values (53, 14054545, "images/14054545_3.jpg");
+insert into IMAGE values (54, 14054545, "images/14054545_4.jpg");
+insert into IMAGE values (55, 14054545, "images/14054545_5.jpg");
+
+insert into FACE_LOG (exam_id, student_id, time, error_type, image, remarks) values (1, 18011529, now(), 1, "log_image/image_1.jpg", "5sec");
+insert into FACE_LOG (exam_id, student_id, time, error_type, image, remarks) values (3, 18011529, now(), 2, "log_image/image_2.jpg", "two people");
+insert into FACE_LOG (exam_id, student_id, time, error_type, image, remarks) values (4, 19022222, now(), 3, "log_image/image_3.jpg", "different person");
+insert into FACE_LOG (exam_id, student_id, time, error_type, image, remarks) values (5, 18055555, now(), 1, "log_image/image_4.jpg", "5sec");
+insert into FACE_LOG (exam_id, student_id, time, error_type, image, remarks) values (3, 17012121, now(), 1, "log_image/image_5.jpg", "4sec");
